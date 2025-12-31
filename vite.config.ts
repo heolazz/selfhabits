803f5673c330f52f5fcf269a4ae0b034e9fb788d
heolazz <heolazz.gmail.com>
2025-12-31 10:52:57 +0700

    fix pwa error download

--- vite.config.ts
+++ vite.config.ts
@@ -5,33 +5,27 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
+    VitePWA({ 
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'Zenith Personal Organizer',
         short_name: 'Zenith',
-        description: 'Simplify your daily routine.',
+        description: 'Organizational tool for daily life',
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'logo.png', // Pastikan nama file sama dengan di public folder
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'logo.png',
             sizes: '512x512',
             type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
           }
         ]
       }
     })
-  ]
+  ],
 })
